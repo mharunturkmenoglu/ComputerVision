0e7c721dea92d162c9fe3b36a476d6691f56d6a7
mharunturkmenoglu <mht9811@gmail.com>
2022-09-15 13:18:00 +0300

    feat: add fullscreen to main

--- ExtendImage/main.py
+++ ExtendImage/main.py
@@ -1,12 +1,15 @@
 from operator import concat
 import cv2
-from extendImage import ExtendImage
+from ExtendImage import ExtendImage
 from concatenateImages import ConcatenateImages
 
 def main():
     concat = ConcatenateImages("images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg")
     img = concat.concatenate()
-    cv2.imshow("image", img)
+
+    cv2.namedWindow("window1", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("window1",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    cv2.imshow("window1", img)
     cv2.waitKey(0)
     
 if __name__ == "__main__":
